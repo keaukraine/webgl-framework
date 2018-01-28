30e60218050095f9cb140e72a12bac7c9117e159
keaukraine <keaukraine@gmail.com>
2018-01-28 18:43:08 +0200

    Added texture params to UncompressedTextureLoader.

--- src/UncompressedTextureLoader.js
+++ src/UncompressedTextureLoader.js
@@ -13,7 +13,7 @@ define(function () {
      * @param  {Function} callbak - callback called after texture is loaded to GPU
      * @return {number} - WebGL texture
      */
-    UncompressedTextureLoader.load = function (url, callback) {
+    UncompressedTextureLoader.load = function (url, callback, minFilter, magFilter, clamp) {
         return new Promise((resolve, reject) => {
             const texture = gl.createTexture();
 
@@ -22,8 +22,15 @@ define(function () {
             texture.image.onload = () => {
                 gl.bindTexture(gl.TEXTURE_2D, texture);
                 gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.image);
-                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, minFilter || gl.LINEAR);
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, magFilter || gl.LINEAR);
+                if (typeof clamp !== "undefined" && clamp === true) {
+                    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+                    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+                } else {
+                    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
+                    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
+                }
                 gl.bindTexture(gl.TEXTURE_2D, null);
 
                 if (texture.image && texture.image.src) {
